9c818e2524bdc554773067339939588727691045
JoshHyde9 <joshhyde546@gmail.com>
2023-01-19 09:53:51 +1100

    feat: added procedure for getting a single post by id

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,7 +1,13 @@
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     return await ctx.prisma.post.findMany();
   }),
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input, ctx }) => {
+      return await ctx.prisma.post.findUnique({ where: { id: input.id } });
+    }),
 });
