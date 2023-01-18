9f38a6674237eb201e533c1bd0d971632beacba6
JoshHyde9 <joshhyde546@gmail.com>
2023-01-19 09:43:24 +1100

    feat: added post router to trpc

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
+import { postRouter } from "./routers/post";
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "./routers/example";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  post: postRouter,
 });
 
 // export type definition of API
