15205877b6083ea7559fc0ade364badc4caf7ac1
JoshHyde9 <joshhyde546@gmail.com>
2023-01-20 09:45:30 +1100

    feat: user is auto logged in when registered

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,17 +1,32 @@
 import { type NextPage } from "next";
+import { useRouter } from "next/router";
 import { type z } from "zod";
 
 import { api } from "../utils/api";
 import { registerUser } from "../utils/schema";
 
 import { Form } from "../components/Form";
+import { signIn } from "next-auth/react";
 
 const Register: NextPage = () => {
+  const router = useRouter();
+
   const {
     mutate: mutateRegisterUser,
     error,
     isLoading,
-  } = api.user.register.useMutation();
+  } = api.user.register.useMutation({
+    onSuccess: async (_, { username, password }) => {
+      await signIn("credentials", {
+        username,
+        password,
+        redirect: true,
+        callbackUrl: "/",
+      });
+
+      await router.push("/");
+    },
+  });
 
   const handleRegister = (data: z.infer<typeof registerUser>) => {
     mutateRegisterUser(data);
