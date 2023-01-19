8f72c188d82ce7a5caa17aad8c0c78d3f188b77e
JoshHyde9 <joshhyde546@gmail.com>
2023-01-20 10:47:46 +1100

    refactor: decided to add username to session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -50,6 +50,7 @@ export const authOptions: AuthOptions = {
     jwt: ({ token, user }) => {
       if (user) {
         token.userId = user.id;
+        token.username = user.username;
       }
 
       return token;
@@ -57,6 +58,7 @@ export const authOptions: AuthOptions = {
     session: ({ session, token }) => {
       if (token) {
         session.user.userId = token.userId;
+        session.user.username = token.username;
       }
 
       return session;
