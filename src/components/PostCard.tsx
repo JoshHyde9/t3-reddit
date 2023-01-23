bcb8edda33746d1275dbb9f75200c1de900b5f62
JoshHyde9 <joshhyde546@gmail.com>
2023-01-23 13:10:38 +1100

    feat: added post points to card

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,8 +1,20 @@
 import type { Post } from "@prisma/client";
 
-export const PostCard = ({ post }: { post: Post }) => (
+export const PostCard = ({
+  post,
+}: {
+  post: Post & {
+    creator: {
+      id: string;
+      username: string;
+      createdAt: Date;
+    };
+  };
+}) => (
   <div className="my-4 flex flex-col rounded-md border p-4">
+    <span className="text-sm">u/{post.creator.username}</span>
     <h1 className="text-lg font-semibold">{post.title}</h1>
     <p className="pt-4">{post.text}</p>
+    <p>{post.points}</p>
   </div>
 );
