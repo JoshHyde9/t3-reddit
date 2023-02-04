79f9c0ac1d048c087d0fcf8e4e97da0bad7e5b19
JoshHyde9 <joshhyde546@gmail.com>
2023-02-04 11:45:52 +1100

    feat: added page for user account

---
+++ src/pages/user/[username].tsx
@@ -0,0 +1,122 @@
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import superjson from "superjson";
+import { format } from "date-fns";
+
+import { appRouter } from "../../server/api/root";
+import { createInnerTRPCContext } from "../../server/api/trpc";
+import { prisma } from "../../server/db";
+
+import { api } from "../../utils/api";
+
+import { PostCard } from "../../components/post/PostCard";
+
+export const getStaticProps = async (
+  context: GetStaticPropsContext<{ username: string }>
+) => {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: createInnerTRPCContext({ session: null }),
+    transformer: superjson,
+  });
+
+  const username = context.params?.username as string;
+
+  await ssg.user.getAllUserPosts.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username,
+    },
+    revalidate: 1,
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await prisma.post.findMany({
+    select: { creator: { select: { username: true } } },
+  });
+
+  return {
+    paths: posts.map((post) => ({
+      params: {
+        username: post.creator.username,
+      },
+    })),
+    fallback: "blocking",
+  };
+};
+
+const UserAccount = (
+  props: InferGetServerSidePropsType<typeof getStaticProps>
+) => {
+  const userPostsQuery = api.user.getAllUserPosts.useQuery(
+    { username: props.username },
+    { refetchOnWindowFocus: false }
+  );
+
+  if (userPostsQuery.status !== "success") {
+    return <p>Loading...</p>;
+  }
+
+  const { data: userPosts } = userPostsQuery;
+
+  if (!userPosts) {
+    return (
+      <section className="my-4 mx-auto max-w-4xl">
+        <p>User does not exist.</p>
+      </section>
+    );
+  }
+
+  if (userPosts.posts.length <= 0) {
+    return (
+      <section className="my-4 mx-auto max-w-4xl">
+        <p>User hasn&apos;t created any posts.</p>
+      </section>
+    );
+  }
+
+  return (
+    <section className="my-4 mx-auto max-w-4xl">
+      <section className="flex justify-between gap-x-4">
+        <section className="w-9/12">
+          {userPosts.posts.map((post) => (
+            <PostCard
+              key={post.id}
+              post={post}
+              voteStatus={post.votes?.find(
+                (status) => post.id === status.postId
+              )}
+            />
+          ))}
+        </section>
+        <section className="my-4 h-fit min-w-[250px] max-w-xs rounded-md border p-4">
+          <h1 className="mb-4 text-lg font-semibold">{userPosts.username}</h1>
+          <div className="flex justify-between">
+            <div>
+              <h2>Cake day</h2>
+              <p className="font-light text-neutral-500">
+                {format(userPosts.createdAt, "MMM dd, YYY")}
+              </p>
+            </div>
+            <div>
+              <h2>Cake day</h2>
+              <p className="font-light text-neutral-500">
+                {format(userPosts.createdAt, "MMM dd, YYY")}
+              </p>
+            </div>
+          </div>
+          <hr className="my-4" />
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default UserAccount;
