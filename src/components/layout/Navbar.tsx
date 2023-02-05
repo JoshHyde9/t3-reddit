04e7be5aa2f5b671b0cbdb8f9de46274f5328151
JoshHyde9 <joshhyde546@gmail.com>
2023-02-05 11:30:51 +1100

    feat: added search

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import { type Session } from "next-auth";
+import React, { type ChangeEvent, useState, useEffect, useRef } from "react";
+import Link from "next/link";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
 import { signOut, useSession } from "next-auth/react";
-import { type Session } from "next-auth";
+
+import { api } from "../../utils/api";
 
 const NavLink = ({
   to,
@@ -86,13 +89,53 @@ const MobileNav = ({
   );
 };
 
+function useOutsideAlerter(
+  ref: React.RefObject<HTMLInputElement>,
+  setDropDown: React.Dispatch<React.SetStateAction<boolean>>
+) {
+  useEffect(() => {
+    // Close dropdown if clicked outside
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setDropDown(false);
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, setDropDown]);
+}
+
 const NavBar: React.FC = () => {
   const [open, setOpen] = useState(false);
   const { data: session } = useSession();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [dropDown, setDropDown] = useState(false);
+
+  const wrapperRef = useRef<HTMLInputElement>(null);
+  useOutsideAlerter(wrapperRef, setDropDown);
+
+  const { mutate: search, data } = api.search.searchAll.useMutation();
+
+  const handleSearchTerm = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = (event: ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    search({ searchTerm });
+    setDropDown(true);
+  };
+
   return (
     <nav className="flex h-20 w-screen items-center px-4 py-4">
       <MobileNav open={open} setOpen={setOpen} session={session} />
-      <div className="flex w-full items-center justify-end md:pr-10">
+
+      <div className="flex w-full items-center md:pr-10">
         <div
           className="relative z-50 flex h-8 w-8 flex-col items-center justify-between md:hidden"
           onClick={() => {
@@ -117,7 +160,55 @@ const NavBar: React.FC = () => {
           />
         </div>
 
-        <div className="mt-10 hidden items-center md:flex">
+        <div className="mt-10 hidden grow items-center md:flex">
+          <div className="relative flex">
+            <form className="w-full" onSubmit={handleSearch}>
+              <input
+                value={searchTerm}
+                onChange={handleSearchTerm}
+                type="text"
+                className="w-full rounded-md border-2 border-teal-600 py-1 px-2 focus:outline-none"
+                placeholder="Search Reddit"
+                onClick={() => setDropDown(true)}
+              />
+            </form>
+            {dropDown ? (
+              <div
+                className="absolute z-10 w-full translate-y-8 rounded-md rounded-t-none border-2 border-teal-600 bg-white px-2 pt-4"
+                ref={wrapperRef}
+              >
+                {data && (
+                  <>
+                    <h1 className="font-semibold">Communities</h1>
+                    <hr className="my-2" />
+                    <div onClick={() => setDropDown(false)}>
+                      {data.subs.map((sub, i) => (
+                        <div key={i} className="cursor-pointer p-1">
+                          <Link href={`/r/${sub.name}`}>r/{sub.name}</Link>
+                        </div>
+                      ))}
+                    </div>
+                    <h1 className="font-semibold">People</h1>
+                    <hr className="my-2" />
+                    <div onClick={() => setDropDown(false)}>
+                      {data.users.map((user, i) => (
+                        <div key={i}>
+                          <div key={i} className="cursor-pointer p-1">
+                            <Link href={`/user/${user.username}`}>
+                              u/{user.username}
+                            </Link>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                )}
+              </div>
+            ) : null}
+          </div>
+        </div>
+
+        <div className="mt-10 ml-auto hidden items-center md:flex">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/all">All</NavLink>
           {!session && (
