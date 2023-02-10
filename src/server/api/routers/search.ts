256b4f076a990d4756f4e10efa97374327cb6653
JoshHyde9 <joshhyde546@gmail.com>
2023-02-11 09:17:36 +1100

    fix: types for search results

--- src/server/api/routers/search.ts
+++ src/server/api/routers/search.ts
@@ -1,3 +1,4 @@
+import type { Sub, User } from "@prisma/client";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
@@ -8,10 +9,12 @@ export const searchRouter = createTRPCRouter({
     .mutation(async ({ input, ctx }) => {
       const query = `%${input.searchTerm}%`;
       const [subs, users] = await ctx.prisma.$transaction([
-        ctx.prisma
-          .$queryRaw`SELECT name FROM "Sub" WHERE "name" ILIKE ${query} LIMIT 10;`,
-        ctx.prisma
-          .$queryRaw`SELECT username FROM "User" WHERE "username" ILIKE ${query} LIMIT 10;`,
+        ctx.prisma.$queryRaw<
+          Sub[]
+        >`SELECT name FROM "Sub" WHERE "name" ILIKE ${query} LIMIT 10;`,
+        ctx.prisma.$queryRaw<
+          User[]
+        >`SELECT username FROM "User" WHERE "username" ILIKE ${query} LIMIT 10;`,
       ]);
       return { subs, users };
     }),
