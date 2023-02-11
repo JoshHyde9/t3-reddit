cb47fc4dfc30c4d32b720e4b0fed29c9e530d271
JoshHyde9 <joshhyde546@gmail.com>
2023-02-12 08:32:02 +1100

    fix: removed warning

--- src/components/layout/LoadingSpinner.tsx
+++ src/components/layout/LoadingSpinner.tsx
@@ -11,7 +11,7 @@ export const LoadingSpinner = () => (
         cx="12"
         cy="12"
         r="10"
-        stroke-width="4"
+        strokeWidth="4"
       ></circle>
       <path
         className="opacity-75"
