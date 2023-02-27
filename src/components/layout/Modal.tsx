d652266aa4029f107fc1355e9c83bc01c9933a17
JoshHyde9 <joshhyde546@gmail.com>
2023-02-27 16:53:11 +1100

    refactor: added more options

--- src/components/layout/Modal.tsx
+++ src/components/layout/Modal.tsx
@@ -5,6 +5,8 @@ type ModalProps = {
   title: string;
   message: string;
   showModal: boolean;
+  buttonMessage: string;
+  type: "success" | "danger";
   functionParams: { username: string; subName: string };
   setShowModal: Dispatch<SetStateAction<boolean>>;
   handleFunction: UseMutateFunction<
@@ -18,6 +20,8 @@ export const Modal = ({
   title,
   message,
   setShowModal,
+  buttonMessage,
+  type,
   functionParams,
   handleFunction,
 }: ModalProps) => {
@@ -35,9 +39,15 @@ export const Modal = ({
           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
-                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                <div
+                  className={`mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full ${
+                    type === "danger" ? "bg-red-100" : "bg-teal-100"
+                  } sm:mx-0 sm:h-10 sm:w-10`}
+                >
                   <svg
-                    className="h-6 w-6 text-red-600"
+                    className={`h-6 w-6 ${
+                      type === "danger" ? "text-red-600" : "text-teal-600"
+                    }`}
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
@@ -52,10 +62,7 @@ export const Modal = ({
                   </svg>
                 </div>
                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                  <h3
-                    className="text-base font-semibold leading-6 text-gray-900"
-                    id="modal-title"
-                  >
+                  <h3 className="text-base font-semibold leading-6 text-gray-900">
                     {title}
                   </h3>
                   <div className="mt-2">
@@ -67,7 +74,11 @@ export const Modal = ({
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button
                 type="button"
-                className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                className={`inline-flex w-full justify-center rounded-md border border-transparent  ${
+                  type === "danger"
+                    ? "bg-red-600 hover:bg-red-700 focus:ring-red-500"
+                    : "bg-teal-600 hover:bg-teal-700 focus:ring-teal-500"
+                } px-4 py-2 text-base font-medium text-white shadow-sm  focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm`}
                 onClick={() =>
                   handleFunction({
                     username: functionParams.username,
@@ -75,7 +86,7 @@ export const Modal = ({
                   })
                 }
               >
-                Promote
+                {buttonMessage}
               </button>
               <button
                 type="button"
